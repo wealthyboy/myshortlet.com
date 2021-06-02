10cfc01e127c6c96831a9147690349a0b1efdc13
Atam Jacob <jacob.atam@gmail.com>
2021-06-02 08:13:24 -0700

    Added no dev

--- database/migrations/2021_02_18_113855_create_requirements_table.php
+++ database/migrations/2021_02_18_113855_create_requirements_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReservationRequirementsTable extends Migration
+class CreateRequirementsTable extends Migration
 {
     /**
      * Run the migrations.
