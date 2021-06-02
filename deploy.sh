99b904d6ea774ad5dcbad371e62d0239ebcb1b31
Atam Jacob <jacob.atam@gmail.com>
2021-06-02 07:15:31 -0700

    Added no dev

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ php artisan down || true
 git pull origin master
 
 # Install/update composer dependecies
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
 # Run database migrations
 php artisan migrate --force
