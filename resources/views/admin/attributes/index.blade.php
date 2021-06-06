d25150930f64c89decfd444bc7d56ca040e2080a
Atam Jacob <jacob.atam@gmail.com>
2021-06-06 10:12:38 -0700

    Updated attributes

--- resources/views/admin/attributes/index.blade.php
+++ resources/views/admin/attributes/index.blade.php
@@ -53,7 +53,7 @@
                             <option  value="" selected="">--Choose Parent--</option>
                                 @foreach($attributes as $attribute)
                                     <option class="" value="{{ $attribute->id }}" >{{ $attribute->name }} </option>
-                                    @include('includes.product_attr',['disabled'=>true ,'attribute'=>$attribute])
+                                    @include('includes.product_attr',['attributes'=>$attribute])
                                 @endforeach
                             </select>
                         </div>
