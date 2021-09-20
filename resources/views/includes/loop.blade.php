b5006066e642520ecdc5c37d6c039e6c553905c6
Atam Jacob <jacob.atam@gmail.com>
2021-09-20 06:33:59 -0700

    Updated propery

--- resources/views/includes/loop.blade.php
+++ resources/views/includes/loop.blade.php
@@ -1,16 +1,6 @@
 
 @if ($obj->children->count())
 @foreach($obj->children as $obj)
-    <div class="togglebutton ml-3">
-        <label>
-            <input  
-                {{ $helper->check(optional($model)->attributes , $obj->id) ? 'checked' : '' }} 
-                name="attribute_id[]"  value="{{ $obj->id }}" type="checkbox" 
-            >
-        {{ $obj->name }}
-        </label>
-    </div>
-     
     @include('includes.loop',['obj'=>$obj])
 @endforeach
 
