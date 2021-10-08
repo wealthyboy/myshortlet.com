d2e89be3609a06358d94c7391b9190a33d35b564
Atam Jacob <jacob.atam@gmail.com>
2021-10-08 09:12:23 -0700

    Updated coming soon

--- resources/views/underconstruction/index.blade.php
+++ resources/views/underconstruction/index.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   
-  <title>{{ Config('app.name') }}  Coming soon</title>
+  <title>Avm  Coming soon</title>
         <meta name="author" content="AchuWorld">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
