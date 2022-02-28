1afbd40d9c0540f20b0e3c3107be4bdaa8d72bbe
Atam Jacob <jacob.atam@gmail.com>
2022-02-27 16:00:33 -0800

    Updated Webhook

--- config/app.php
+++ config/app.php
@@ -56,6 +56,9 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'google_map' => env('GOOGLE_MAP', null) ,
+
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
