848e0f2583bccd15afda17d68e7a6e36ab3bfb41
Seth Atam <seth.atam@gmail.com>
2023-01-11 12:40:06 +0100

    U

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ use App\Models\Banner;
 
 use App\Models\SystemSetting;
 use App\Models\Http\Helper;
-
+use App\Models\User;
 
 class HomeController
 {
@@ -25,17 +25,20 @@ class HomeController
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index(Request $request)
-    {    
+    {
+
+        $user  = User::where('email', 'jacob.atam@gmail.com')->first();
+        dd($user);
         $site_status = Live::first();
         $banners =  Banner::banners()->get();
 
 
-        if (!$site_status->make_live ) {
-            return view('index',compact('banners')); 
+        if (!$site_status->make_live) {
+            return view('index', compact('banners'));
         } else {
             //Show site if admin is logged in
-            if ( auth()->check()  && auth()->user()->isAdmin()){
-                return view('index',compact('banners')); 
+            if (auth()->check()  && auth()->user()->isAdmin()) {
+                return view('index', compact('banners'));
             }
             return view('underconstruction.index');
         }
@@ -50,26 +53,21 @@ class HomeController
 
         $states      = Location::where('location_type', 'state')->has('properties')->latest()->get();
         $cities      = Location::where('location_type', 'city')->has('properties')->latest()->get();
-        $featureds   = Property::where('featured',true)->take(4)->get();
-        $posts       = Information::orderBy('created_at','DESC')->where('blog',true)->take(3)->get();
-        $banners = Banner::where('type','banner')->orderBy('sort_order','asc')->get();
-        $sliders = Banner::where('type','slider')->orderBy('sort_order','asc')->get();
+        $featureds   = Property::where('featured', true)->take(4)->get();
+        $posts       = Information::orderBy('created_at', 'DESC')->where('blog', true)->take(3)->get();
+        $banners = Banner::where('type', 'banner')->orderBy('sort_order', 'asc')->get();
+        $sliders = Banner::where('type', 'slider')->orderBy('sort_order', 'asc')->get();
 
 
         $saved =  auth()->check() ? auth()->user()->favorites->pluck('property_id')->toArray() : [];
-        if (!$site_status->make_live ) {
-            return view('index',compact('sliders','banners','states','posts','featureds','cities','saved'));
+        if (!$site_status->make_live) {
+            return view('index', compact('sliders', 'banners', 'states', 'posts', 'featureds', 'cities', 'saved'));
         } else {
             //Show site if admin is logged in
-            if ( auth()->check()  && auth()->user()->isAdmin()){
-                return view('index',compact('sliders','banners','states','posts','featureds','cities','saved'));
+            if (auth()->check()  && auth()->user()->isAdmin()) {
+                return view('index', compact('sliders', 'banners', 'states', 'posts', 'featureds', 'cities', 'saved'));
             }
             return view('underconstruction.index');
-        } 
+        }
     }
-
-
-
-
-    
 }
