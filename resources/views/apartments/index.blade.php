18e1155074017ff93dc77e71292518cc7ff8b2a2
Seth Atam <seth.atam@gmail.com>
2023-01-11 21:20:49 +0100

    U

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -3,7 +3,7 @@
 <div class="">
    <div class="container">
       <div class="row">
-         <div class="col-md-12 ml-auto mr-auto mt-4">
+         <div class="col-md-12 ml-auto mr-auto">
             <category-search :reload="0" />
          </div>
       </div>
@@ -25,10 +25,9 @@
          </div>
       </div>
       @else
-      <div class="col-md-2 pl-2">
-         <a href="" class="h">
-            <img class="img-fluid" src="https://avenuemontaigne.ng/uploads/Lfr25dG9jV42zCJ3iDQLymqDYOhzYYfCQi0ddaYn.gif" alt="">
-         </a>
+      <div class="col-md-2 pr-2 mobile-sidebar">
+         <div class=" bg-white  sidebar-section">
+         </div>
       </div>
       <div id="load-products" class="col-md-8 pl-1">
          <products-index :next_page="{{ collect($next_page) }}" :propertys="{{ $properties->load('facilities','free_services') }}" />
@@ -36,7 +35,7 @@
       @endif
 
       <div class="col-md-2 pl-2">
-         <a href="" class="h">
+         <a href="https://theluxurysale.com" class="h">
             <img class="img-fluid" src="https://avenuemontaigne.ng/uploads/Lfr25dG9jV42zCJ3iDQLymqDYOhzYYfCQi0ddaYn.gif" alt="">
          </a>
       </div>
