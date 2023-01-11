ea8f61d0ad8050b681eda7792820d0ceb7807386
Seth Atam <seth.atam@gmail.com>
2023-01-11 11:45:34 +0100

    U

--- resources/views/_partials/login_modal.blade.php
+++ resources/views/_partials/login_modal.blade.php
@@ -2,7 +2,7 @@
    @csrf
    <div class="text-center">
       <h2>Login</h2>
-      <p class="">Sign in to unlock the best of Royalbnb Properties.</p>
+      <p class="">Sign in to unlock the best of AVM.</p>
    </div>
    <div class="form-group bmd-form-group">
       <label class="bmd-label-floating">Email address</label>
@@ -18,30 +18,30 @@
    <div class="d-flex justify-content-between align-items-center">
       <div class="remeber">
          <div class="checkbox  flex-grow-1">
-            <label  id="box50" class="checkbox-label">
-            <input for="box50" name="remember_me" value="1" class="" type="checkbox">
-            <span class="checkbox-custom rectangular"></span>
-            <span class="checkbox-label-text">Remember </span> 
+            <label id="box50" class="checkbox-label">
+               <input for="box50" name="remember_me" value="1" class="" type="checkbox">
+               <span class="checkbox-custom rectangular"></span>
+               <span class="checkbox-label-text">Remember </span>
             </label>
          </div>
       </div>
-      
+
       <div class="text-right mt-2">
-         <a  class="bold"  href="/password/reset">Forget your password?</a>
+         <a class="bold" href="/password/reset">Forget your password?</a>
       </div>
    </div>
    <div class="clearfix"></div>
    <p class="form-group mt-3">
-      <button type="submit"   class="ml-1 btn btn-primary btn-round  btn-block auth-form-button">
+      <button type="submit" class="ml-1 btn btn-primary btn-round  btn-block auth-form-button">
          <div class="auth-spinner d-none">
             @include('_partials.spinner',['bgcolor' => '#ffffff'])
-         </div> 
-        <span class="lt bold text-white bold">Login</span> 
+         </div>
+         <span class="lt bold text-white bold">Login</span>
       </button>
    </p>
    <div class="mt-4 pt-4 text-center border-top">
       <p class="form-group col-12">
-         Dont have an account yet?  <a class="auth-form bold" data-to="register" href="#">Create One</a>
+         Dont have an account yet? <a class="auth-form bold" data-to="register" href="#">Create One</a>
       </p>
       </p>
    </div>
