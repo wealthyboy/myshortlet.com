501db31184db7d7ffec468ba06db72496a160c80
Jacob <jacob.atam@gmail.com>
2023-07-11 08:20:45 -0700

    Fix

--- resources/views/_partials/header_styles.blade.php
+++ resources/views/_partials/header_styles.blade.php
@@ -1,46 +1,56 @@
 <head>
-      <meta charset="utf-8" />
-      <title>{{ isset( $page_title) ?  $page_title .' |  '.config('app.name') :  $system_settings->meta_title  }}</title>
-      <meta name="csrf-token" content="{{ csrf_token() }}">
-      <meta name="description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta name="keywords" content="{{ isset($system_settings->meta_tag_keywords) ? $system_settings->meta_tag_keywords : 'cleanse,detox,flattummy,flattummy tea ng,slimming tea' }}" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="canonical" href="{{ Config('app.url') }}">
-      <!-- Favicone Icon -->
-      <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
-      <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
-      <link rel="icon" type="image/png" href="/img/favicon-96x96.png">
-      <link rel="apple-touch-icon" href="/img/favicon-96x96.png">
-      <!-- CSS -->
-      <!-- Main CSS File -->
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" rel="stylesheet">
+   <meta charset="utf-8" />
+   <title>{{ isset( $page_title) ?  $page_title .' |  '.config('app.name') :  $system_settings->meta_title  }}</title>
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+   <meta name="description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta name="keywords" content="{{ isset($system_settings->meta_tag_keywords) ? $system_settings->meta_tag_keywords : 'cleanse,detox,flattummy,flattummy tea ng,slimming tea' }}" />
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="canonical" href="{{ Config('app.url') }}">
+   <!-- Favicone Icon -->
+   <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
+   <link rel="icon" href="/images/favicon.ico" type="image/x-icon">
+   <link rel="icon" type="image/png" href="/img/favicon-96x96.png">
+   <link rel="apple-touch-icon" href="/img/favicon-96x96.png">
+   <!-- CSS -->
+   <!-- Main CSS File -->
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-      <link href="/css/services_style.css?version={{ str_random(6) }}" rel="stylesheet">
-      <link href="/css/banner.css?version={{ str_random(6) }}" rel="stylesheet">
 
-      @yield('page-css')
-      <meta property="og:site_name" content="avenuemontaigne.com">
-      <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link href="/css/services_style.css?version={{ str_random(6) }}" rel="stylesheet">
+   <link href="/css/banner.css?version={{ str_random(6) }}" rel="stylesheet">
 
-      <meta property="og:url" content="https://avenuemontaigne.ng/">
-      <meta property="og:title" content="avenuemontaigne">
-      <meta property="og:type" content="website">
-      <meta property="og:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta property="og:image:alt" content="">
-      <meta name="twitter:site" content="@avenuemontaigne">
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <meta name="twitter:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
-      <script src="/js/popper.min.js"></script>
+   @yield('page-css')
+   <meta property="og:site_name" content="avenuemontaigne.com">
+   <link rel="preconnect" href="https://fonts.googleapis.com">
 
-      <script>
-         Window.user = {
-         	user: {!! auth()->check() ? auth()->user() : 0000 !!},
-         	loggedIn: {!! auth()->check() ? 1 : 0 !!},
-         	settings: {!! isset($system_settings) ? $system_settings : '' !!},
-         	token: '{!! csrf_token() !!}',
-            request: {!! collect(request()->all())  ?  collect(request()->all()) : null !!}
+   <meta property="og:url" content="https://avenuemontaigne.ng/">
+   <meta property="og:title" content="avenuemontaigne">
+   <meta property="og:type" content="website">
+   <meta property="og:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta property="og:image:alt" content="">
+   <meta name="twitter:site" content="@avenuemontaigne">
+   <meta name="twitter:card" content="summary_large_image">
+   <meta name="twitter:title" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <meta name="twitter:description" content="{{ isset($page_meta_description) ? $page_meta_description : $system_settings->meta_description }}">
+   <script src="/js/popper.min.js"></script>
+
+   <script>
+      Window.user = {
+         user: {
+            !!auth() - > check() ? auth() - > user() : 0000!!
+         },
+         loggedIn: {
+            !!auth() - > check() ? 1 : 0!!
+         },
+         settings: {
+            !!isset($system_settings) ? $system_settings : ''!!
+         },
+         token: '{!! csrf_token() !!}',
+         request: {
+            !!collect(request() - > all()) ? collect(request() - > all()) : null!!
          }
-      </script>
-   </head>
+      }
+   </script>
+</head>
