aca38a965aebdafb253ef6ec09b9741100fdeb8a
Jacob <jacob.atam@gmail.com>
2023-07-15 09:18:48 -0700

    Fix

--- resources/js/components/properties/Guests.vue
+++ resources/js/components/properties/Guests.vue
@@ -13,38 +13,23 @@
         </div>
       </div>
     </div>
-    <div
-      style="min-width: 100%; z-index: 2; background: #ffffff; opacity: 0;"
-      id="people-dropdown"
-      class="people-dropdown d-none pl-4 pt-3 pb-4 pr-4 position-absolute  border"
-    >
+    <div style="min-width: 100%; z-index: 2; background: #ffffff; opacity: 0;" id="people-dropdown"
+      class="people-dropdown d-none pl-4 pt-3 pb-4 pr-4 position-absolute  border">
       <div class=" d-flex p-2 justify-content-between">
         <div class="text-left">
           <div class="" id="">Adults</div>
           <div class="text-muted " id="">Ages 13 or above</div>
         </div>
         <div class="d-flex align-items-center justify-content-between">
-          <button
-            type="button"
-            disabled=""
-            aria-label="decrease value"
-            aria-describedby=""
-            class="mr-3   raised cursor-pointer add-subtract  min-adults"
-            data-name="adults"
-            data-math="minus"
-          >
+          <button type="button" disabled="" aria-label="decrease value" aria-describedby=""
+            class="mr-3   raised cursor-pointer add-subtract  min-adults" data-name="adults" data-math="minus">
             <span class=""><i class="fas fa-minus"></i></span>
           </button>
           <div class="">
             <span aria-hidden="true" class="adults">{{ adults }}</span>
           </div>
-          <button
-            data-math="add"
-            data-name="adults"
-            data-number="1"
-            class="ml-3 raised cursor-pointer add-subtract"
-            type="button"
-          >
+          <button data-math="add" data-name="adults" data-number="1" class="ml-3 raised cursor-pointer add-subtract"
+            type="button">
             <span class="">
               <i class="far fa-plus"></i>
             </span>
@@ -58,28 +43,16 @@
           <div class="text-muted" id="">0 to 13</div>
         </div>
         <div class="d-flex align-items-center justify-content-between">
-          <button
-            type="button"
-            disabled=""
-            aria-label="decrease value"
-            aria-describedby=""
-            class="mr-3 raised min-children cursor-pointer  add-subtract"
-            data-name="children"
-            data-math="minus"
-          >
+          <button type="button" disabled="" aria-label="decrease value" aria-describedby=""
+            class="mr-3 raised min-children cursor-pointer  add-subtract" data-name="children" data-math="minus">
             <span><i class="fas fa-minus"></i></span>
           </button>
           <div class="">
             <span class="children" aria-hidden="true">{{ children }}</span>
           </div>
 
-          <button
-            data-math="add"
-            data-number="1"
-            data-name="children"
-            class="ml-3 raised cursor-pointer add-subtract"
-            type="button"
-          >
+          <button data-math="add" data-number="1" data-name="children" class="ml-3 raised cursor-pointer add-subtract"
+            type="button">
             <span class="">
               <i class="far fa-plus"></i>
             </span>
@@ -93,28 +66,16 @@
           <div class="text-muted " id=""></div>
         </div>
         <div class="d-flex align-items-center justify-content-between">
-          <button
-            type="button"
-            disabled=""
-            aria-label="decrease value"
-            aria-describedby=""
-            data-math="minus"
-            data-name="rooms"
-            class="mr-3 raised min-rooms cursor-pointer add-subtract"
-          >
+          <button type="button" disabled="" aria-label="decrease value" aria-describedby="" data-math="minus"
+            data-name="rooms" class="mr-3 raised min-rooms cursor-pointer add-subtract">
             <span class=""><i class="fas fa-minus"></i></span>
           </button>
           <div class="">
             <span class="rooms" aria-hidden="true">{{ rooms }}</span>
           </div>
 
-          <button
-            data-math="add"
-            data-number="0"
-            data-name="rooms"
-            class="ml-3 raised cursor-pointer add-subtract"
-            type="button"
-          >
+          <button data-math="add" data-number="0" data-name="rooms" class="ml-3 raised cursor-pointer add-subtract"
+            type="button">
             <span class="">
               <i class="far fa-plus"></i>
             </span>
@@ -122,27 +83,9 @@
         </div>
       </div>
 
-      <input
-        type="hidden"
-        class="location-search"
-        name="rooms"
-        :value="rooms"
-        id="rooms"
-      />
-      <input
-        type="hidden"
-        class="location-search"
-        name="children"
-        :value="children"
-        id="children"
-      />
-      <input
-        type="hidden"
-        class="location-search"
-        name="adults"
-        :value="adults"
-        id="adults"
-      />
+      <input type="hidden" class="location-search" name="rooms" :value="rooms" id="rooms" />
+      <input type="hidden" class="location-search" name="children" :value="children" id="children" />
+      <input type="hidden" class="location-search" name="adults" :value="adults" id="adults" />
     </div>
   </div>
 </template>
@@ -151,9 +94,9 @@ export default {
   data() {
     return {
       guests: 0,
-      rooms: this.$root.request.rooms || 1,
-      children: this.$root.request.children || 0,
-      adults: this.$root.request.adults || 1,
+      rooms: nu || 1, ll
+      children: null || 0,
+      adults: null || 1,
     };
   },
 };
