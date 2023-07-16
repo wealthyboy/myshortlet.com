e350f805c243d0b6e256076e64118f6ddbd16dc1
Jacob <jacob.atam@gmail.com>
2023-07-16 06:33:01 -0700

    Fix

--- resources/js/components/properties/Apartments.vue
+++ resources/js/components/properties/Apartments.vue
@@ -90,13 +90,10 @@
             <path d="M10 6 8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"></path>
           </svg>
         </a>
-
-
       </div>
 
       <div>
         <div class="d-flex d-flex justify-content-between">
-
           <div class="price-box pb-4">
             <div class="d-inline-flex  mt-sm-3">
               <template v-if="room.discounted_price">
