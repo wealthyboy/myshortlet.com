9c0c240f4cde98b85249604d81b7656f2defeef1
Jacob <jacob.atam@gmail.com>
2023-11-20 05:36:56 -0800

    fix reao

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -9,11 +9,20 @@
         @if ($cities->children->count())
 
         @foreach($cities->children as $city)
-
+        @if ($city->image === '' || null == $city->image) @continue @endif
         <div class="col-md-6 my-4">
             <a href="/apartments/in/{{ $city->slug }}">
                 <img src="{{ $city->image }}" class="img-fluid" alt="">
+
+                <p>
+                    {{ $city->name }}
+                </p>
+
+                <p>
+                    {{ $city->description }}
+                </p>
             </a>
+
         </div>
         @endforeach
 
