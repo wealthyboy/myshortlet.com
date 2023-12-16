57d78822d12ebb95ee257418020fa7afbd7bd1ba
Jacob <jacob.atam@gmail.com>
2023-12-16 15:23:19 -0800

    fix

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -16,10 +16,10 @@
                 <img src="{{ $city->image }}" class="img-fluid rounded" alt="">
                 <div class="position-absolute top-0 start-0 w-100 h-100 rounded bg-black opacity-50"></div>
                 <div class="position-absolute bottom-0 text-white text-left ml-3 mb-3">
-                    <h2 class="bold-2  text-white fs-5 fs-md-4">
+                    <h2 class="bold-3  text-white fs-5 fs-md-4">
                         {{ $city->name }}
                     </h2>
-                    <p>
+                    <p class="bold-2 text-white">
                         {{ $city->description }}
                     </p>
                 </div>
