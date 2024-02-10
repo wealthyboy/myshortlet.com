6f57dce672be6c968b10695012d512ef22aa7ec4
Jacob <jacob.atam@gmail.com>
2024-02-09 19:31:44 -0800

    fix

--- app/Http/Controllers/Admin/Image/ImagesController.php
+++ app/Http/Controllers/Admin/Image/ImagesController.php
@@ -13,11 +13,11 @@ class ImagesController extends Controller
 {
     protected $settings;
 
-    protected $folders = ['attributes','category','reviews','banners','blog','uploads','apartments','locations'];
-    
+    protected $folders = ['attributes', 'category', 'reviews', 'banners', 'blog', 'uploads', 'apartments', 'locations'];
+
     public function __construct()
-    {	  
-	  $this->settings =  SystemSetting::first();
+    {
+        $this->settings =  SystemSetting::first();
     }
 
 
@@ -25,13 +25,12 @@ class ImagesController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
-    */
+     */
     public function index()
     {
-        
     }
 
-   
+
     /**
      * Store a newly created resource in storage.
      *
@@ -39,14 +38,14 @@ class ImagesController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {   
-        
+    {
+
         //All the apps image goes  to the image table
-        if ( $request->filled('image_id') && $request->image_id !== 'undefined') {  
+        if ($request->filled('image_id') && $request->image_id !== 'undefined') {
             $this->update($request);
         }
         $path = $this->uploadImage($request);
-        return response()->json(['path'=>$path]);
+        return response()->json(['path' => $path]);
     }
 
     /**
@@ -90,92 +89,91 @@ class ImagesController extends Controller
 
     public function uploadImage(Request $request)
     {
-         
-        if($request->hasFile('file')){
+
+        if ($request->hasFile('file')) {
 
             //when the user clicks change remove the previuos image
             request()->validate([
-               'file' => 'required|image|mimes:jpeg,png,webp,jpg,gif,webp',
-               'folder' => 'required'
+                'file' => 'required|image|mimes:jpeg,png,webp,jpg,gif,webp',
+                'folder' => 'required'
             ]);
 
-            if (!in_array($request->folder,$this->folders)){
+            if (!in_array($request->folder, $this->folders)) {
                 return response()->json([
-                    'error' => $request->folder .' Not allowed'
-                ],500);
+                    'error' => $request->folder . ' Not allowed'
+                ], 500);
             }
 
-        
-            $path    =  public_path('images/'. $request->folder );
-            $path_m  =  public_path('images/'. $request->folder.'/m' );
-            $path_tn =  public_path('images/'. $request->folder.'/tn' );
+
+            $path    =  public_path('images/' . $request->folder);
+            $path_m  =  public_path('images/' . $request->folder . '/m');
+            $path_tn =  public_path('images/' . $request->folder . '/tn');
 
 
-            if(!\File::exists($path)){
-                \File::makeDirectory(public_path('images/'. $request->folder ),0755, true);
+            if (!\File::exists($path)) {
+                \File::makeDirectory(public_path('images/' . $request->folder), 0755, true);
             }
 
-            if(!\File::exists($path_m)){
-                \File::makeDirectory(public_path('images/'. $request->folder.'/m' ),0755, true);
+            if (!\File::exists($path_m)) {
+                \File::makeDirectory(public_path('images/' . $request->folder . '/m'), 0755, true);
             }
 
-            if(!\File::exists($path_tn)){
-                \File::makeDirectory(public_path('images/'. $request->folder.'/tn' ),0755, true);
+            if (!\File::exists($path_tn)) {
+                \File::makeDirectory(public_path('images/' . $request->folder . '/tn'), 0755, true);
             }
 
-            $path = $request->file('file')->store('images/'.$request->folder);
+            $path = $request->file('file')->store('images/' . $request->folder);
+
+            dd($path);
             $file = basename($path);
-            $path =  public_path('images/'. $request->folder .'/'.$file);
-        
+            $path =  public_path('images/' . $request->folder . '/' . $file);
+
 
             $img  = \Image::make($path)->fit(400, 400)->save(
-                public_path('images/'. $request->folder .'/m/'.$file)
+                public_path('images/' . $request->folder . '/m/' . $file)
             );
             $canvas = \Image::canvas(106, 145);
-            $image  = \Image::make($path)->resize(150, 250, function($constraint)
-            {
+            $image  = \Image::make($path)->resize(150, 250, function ($constraint) {
                 $constraint->aspectRatio();
             });
             $canvas->insert($image, 'center');
             $canvas->save(
-                public_path('images/'.$request->folder.'/tn/'.$file)
+                public_path('images/' . $request->folder . '/tn/' . $file)
             );
-           return $path = asset('images/'. $request->folder .'/'.$file);
+            return $path = asset('images/' . $request->folder . '/' . $file);
         }
-
     }
 
 
     public static function undo(Request $request)
-    {   
-        $file =basename($request->image_url);
+    {
+        $file = basename($request->image_url);
 
-        $class = '\\App\\Models\\'.$request->model;
-        if( file_exists( public_path('images/'. $request->folder .'/'.$file) ) ) 
-        {   
-            unlink( public_path('images/'. $request->folder .'/'.$file) );
-            unlink( public_path('images/'. $request->folder .'/m/'.$file) );
-            unlink( public_path('images/'. $request->folder .'/tn/'.$file) );
-            if ($request->filled('model') ){
+        $class = '\\App\\Models\\' . $request->model;
+        if (file_exists(public_path('images/' . $request->folder . '/' . $file))) {
+            unlink(public_path('images/' . $request->folder . '/' . $file));
+            unlink(public_path('images/' . $request->folder . '/m/' . $file));
+            unlink(public_path('images/' . $request->folder . '/tn/' . $file));
+            if ($request->filled('model')) {
 
-                if ($request->image_id && $request->filled('type') ){
+                if ($request->image_id && $request->filled('type')) {
                     $model = $class::find($request->image_id);
-                    if ($model){
+                    if ($model) {
                         $model->delete();
                     }
-                    return response(null,200);
+                    return response(null, 200);
                 } else {
                     $model = $class::find($request->image_id);
-                    if ($model){
+                    if ($model) {
                         $model->image = null;
                         $model->save();
                     }
-                    return response(null,200);
+                    return response(null, 200);
                 }
             }
-            return response(null,200);
+            return response(null, 200);
         }
-    } 
+    }
 
     /**
      * Remove the specified resource from storage.
@@ -184,16 +182,11 @@ class ImagesController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function destroy(Request $request)
-    {   
-        if($request->filled('image_url')){
-            if ( self::undo($request)  == true){
-                return response('deleted',200);
+    {
+        if ($request->filled('image_url')) {
+            if (self::undo($request)  == true) {
+                return response('deleted', 200);
             }
         }
     }
-
-    
-
-            
-
 }
