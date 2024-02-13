9695aabc729e167a495ae8ed3f1a37a732d95698
Jacob <jacob.atam@gmail.com>
2024-02-13 12:48:26 -0800

    fix

--- config/mail.php
+++ config/mail.php
@@ -36,7 +36,7 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'), 
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
@@ -84,7 +84,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@avenuemontaigne.ng'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@avenuemontaigne.ng'),
         'name' => env('MAIL_FROM_NAME', 'Avenue Montaigne'),
     ],
 
