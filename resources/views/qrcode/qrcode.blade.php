c3f558d05c15de2e8498a1ef73e76a0650cbfdbb
Jacob <jacob.atam@gmail.com>
2024-02-13 14:54:50 -0800

    fix

--- resources/views/qrcode/qrcode.blade.php
+++ resources/views/qrcode/qrcode.blade.php
@@ -6,8 +6,7 @@
 </head>
 
 <body>
-    <!-- <img src="data:image/svg+xml;base64,{{ base64_encode($qrCode) }}">
-    <p>Scan this QR code to visit: <a href="{{ $url }}">{{ $url }}</a></p> -->
+
 
     <div class="visible-print text-center">
         {!! QrCode::size(100)->generate(Request::url()); !!}
