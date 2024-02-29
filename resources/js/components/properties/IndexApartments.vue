f540ad8115af9d5548c4c6914df28ee832bc594a
Jacob <jacob.atam@gmail.com>
2024-02-29 14:16:59 -0800

    fix

--- resources/js/components/properties/IndexApartments.vue
+++ resources/js/components/properties/IndexApartments.vue
@@ -590,14 +590,14 @@ export default {
             this.showModal = !this.showModal;
             this.room = room
             jQuery(function () {
-                $(".room-carousel").owlCarousel({
+                $(".owl-carousel").owlCarousel({
                     margin: 0,
                     nav: true,
                     dots: true,
-                    navText: [
-                        '<div class="nav-btn prev-slide"><svg  viewBox="0 0 21 40" xmlns="http://www.w3.org/2000/svg"><path d="M19.9 40L1.3 20 19.9 0" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
-                        '<div class="nav-btn next-slide"><svg  viewBox="0 0 19 40" xmlns="http://www.w3.org/2000/svg"><path d="M.1 0l18.6 20L.1 40" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
-                    ],
+                    // navText: [
+                    //     '<div class="nav-btn prev-slide"><svg  viewBox="0 0 21 40" xmlns="http://www.w3.org/2000/svg"><path d="M19.9 40L1.3 20 19.9 0" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
+                    //     '<div class="nav-btn next-slide"><svg  viewBox="0 0 19 40" xmlns="http://www.w3.org/2000/svg"><path d="M.1 0l18.6 20L.1 40" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>',
+                    // ],
                     responsive: {
                         0: {
                             items: 1,
