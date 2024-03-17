22377f88b1726d93f7441cfb37d1a6afdd7b6844
Jacob <jacob.atam@gmail.com>
2024-03-17 10:52:18 -0700

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -156,7 +156,7 @@
 
       </div>
       <div class="row  p-1">
-         <div id="load-products" class="col-md-12">
+         <div id="load-products" class="col-md-12pr ">
             <apartments-index :isGallery="0" :filter="0" :property="{{$property}}" :apartments="{{ $apartments }}" />
          </div>
       </div>
