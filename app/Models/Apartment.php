300084935aa30698ae8a7b8c04c2c1e984ab6b74
Jacob <jacob.atam@gmail.com>
2024-03-20 00:26:00 -0700

    fix

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -228,6 +228,6 @@ class Apartment extends Model
 
     public function getImageGAttribute()
     {
-        return $this->image_m ?? $this->google_drive_image_link[0]
+        return $this->image_m ?? $this->google_drive_image_link[0];
     }
 }
