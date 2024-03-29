cdaa90a8bc7dd8be23201b7db9774db79e80b7e5
Jacob <jacob.atam@gmail.com>
2024-03-28 23:30:17 -0700

    fix

--- app/Http/Controllers/Admin/Properties/PropertiesController.php
+++ app/Http/Controllers/Admin/Properties/PropertiesController.php
@@ -336,11 +336,8 @@ class PropertiesController extends Controller
     public function syncAttributes($request, $apartment, $key = null)
     {
         //  $apartment->attributes()->truncate();
-        $apartment_facilities = ApartmentAttribute::all();
 
-        foreach ($apartment_facilities as  $apartment_facilitie) {
-            $apartment_facilitie->delete();
-        }
+
 
         if (is_array($request->bed_count) && !empty($request->bed_count)) {
             //dd($request->bed_count);
