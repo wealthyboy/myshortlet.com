0a96274b34ca11b72c1bf83e82f12b42bf07d20f
Jacob <jacob.atam@gmail.com>
2024-03-29 19:49:01 -0700

    fix

--- resources/js/components/properties/IndexApartments.vue
+++ resources/js/components/properties/IndexApartments.vue
@@ -238,13 +238,13 @@
                                         <div v-for="(objects, parentName) in apartment_facilities" :key="parentName"
                                             style="margin-bottom: 5rem;" class="col-md-6">
                                             <div class="d-flex align-items-center">
-                                                <svg class="" aria-hidden="true" viewBox="0 0 24 24"
-                                                    xmlns="http://www.w3.org/2000/svg"
+                                                <svg class="uitk-icon uitk-layout-flex-item" aria-hidden="true"
+                                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
                                                     xmlns:xlink="http://www.w3.org/1999/xlink">
-                                                    <path fill-rule="evenodd"
-                                                        d="M20 13V4.83C20 3.27 18.73 2 17.17 2c-.75 0-1.47.3-2 .83l-1.25 1.25c-.16-.05-.33-.08-.51-.08-.4 0-.77.12-1.08.32l2.76 2.76c.2-.31.32-.68.32-1.08 0-.18-.03-.34-.07-.51l1.25-1.25a.828.828 0 0 1 1.41.59V13H2v6c0 1.1.9 2 2 2 0 .55.45 1 1 1h14c.55 0 1-.45 1-1 1.1 0 2-.9 2-2v-6h-2ZM4 19h16v-4H4v4Z"
-                                                        clip-rule="evenodd"></path>
+                                                    <path d="M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z">
+                                                    </path>
                                                 </svg>
+
                                                 <h4 class="ml-2">{{ parentName }}</h4>
                                             </div>
                                             <div class="">
