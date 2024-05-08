0b9b2dd7246b4ea4968d9432639d768d90222957
Jacob <jacob.atam@gmail.com>
2024-05-08 12:14:44 -0700

    fix

--- app/Http/Controllers/SignUp/SignUpController.php
+++ app/Http/Controllers/SignUp/SignUpController.php
@@ -29,7 +29,7 @@ class SignUpController extends Controller
      */
     public function index()
     {
-        $rooms =  Attribute::parents()->where('type', 'room_id')->orderBy('sort_order', 'asc')->get();
+        $rooms =  Attribute::parents()->where('type', 'room_id')->orderBy('sort_order', 'desc')->get();
         return view('checkin.index', compact('rooms'));
     }
 
