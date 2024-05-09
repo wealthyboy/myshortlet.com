0fab815a7721d9c5a7b3a68db76e96519cb0bb03
Jacob <jacob.atam@gmail.com>
2024-05-09 09:32:03 -0700

    fix

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -34,6 +34,8 @@
             <p><strong>Last Name:</strong>{{ $visitor->last_name }}</p>
             <p><strong>Email:</strong>{{ $visitor->email }}</p>
             <p><strong>Phone:</strong>{{ $visitor->phone_number }}</p>
+            <p><strong>Apartment:</strong>{{ $visitor->apartment_id }}</p>
+
             <p><strong>Check-in:</strong> {{ $reservation->checkin->format('l') }} {{ $reservation->checkin->format('d') }} {{ $reservation->checkin->format('F')  }}{{ $reservation->checkin->isoFormat('Y') }}</p>
             <p><strong>Check-out:</strong> {{ $reservation->checkout->format('l') }} {{ $reservation->checkout->format('d') }} {{ $reservation->checkout->format('F')  }}{{ $reservation->checkout->isoFormat('Y') }}</p>
 
