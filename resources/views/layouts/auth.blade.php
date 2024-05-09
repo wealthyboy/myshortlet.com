f0128f11c5d8293e388fc8895b926dea8923d589
Jacob <jacob.atam@gmail.com>
2024-05-09 13:24:21 -0700

    fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -22,6 +22,32 @@
 
 
       @include('_partials.footer')
+      <footer class="      pt-8 pb-6 footer text-muted position-absolute bottom-0 w-100">
+         <div class="container container-xxl">
+
+            <div class="mt-0 mt-md-10 row">
+               <div class="col-md-12 text-center">
+                  <ul class="pull-center">
+                     @foreach($footer_info as $info)
+                     <li>
+                        <a href="/pages/{{ $info->slug }}">
+                           {{ title_case($info->title) }}
+                        </a>
+                     </li>
+                     @endforeach
+                  </ul>
+               </div>
+               <div class="col-md-12 copyright text-center">
+                  <p class="">© Copyright Avenue Montaigne {{ date('Y') }}. All rights reserved.
+                     @if ( auth()->check() && auth()->user()->isAdmin() )
+                     <a target="_blank" class="text-white" href="/admin">Go to Admin</a>
+                     @endif
+                  </p>
+               </div>
+            </div>
+         </div>
+      </footer>
+
    </div>
 
    @include('_partials.modal')
