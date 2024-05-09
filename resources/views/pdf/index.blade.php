df5c4d280ab05559a642743a70e878408e150775
Jacob <jacob.atam@gmail.com>
2024-05-09 10:37:56 -0700

    fix

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -10,24 +10,20 @@
 
 <body style="font-family: Arial, sans-serif;">
 
-    <page backtop="10mm" backbottom="10mm" style="font-size: 14pt">
 
-        <div align="center">
-            <img src="https://avenuemontaigne.ng/images/logo/avnmont-white-04.png" alt="Visitor Image" style="max-width: 300px; max-height: 250px;">
-            <br />
-        </div>
-    </page>
 
     <div class="row" style="display: flex; justify-content: space-between; margin-bottom: 10px; page-break-inside: avoid;">
-        <img src="https://avenuemontaigne.ng/images/logo/avnmont-white-04.png" alt="Visitor Image" style="max-width: 300px; max-height: 250px;">
+        <div class="left" style="width: 45%; text-align: left;">
+            <img src="/images/logo/avm_residences.png" alt="Visitor Image" style="max-width: 300px; max-height: 250px;">
+        </div>
 
         <div class="left" style="width: 45%; text-align: left;">
             <h1>Visitor Details</h1>
-            <p><strong>First Name: </strong>{{ $visitor->first_name }}</p>
-            <p><strong>Last Name:</strong>{{ $visitor->last_name }}</p>
-            <p><strong>Email:</strong>{{ $visitor->email }}</p>
-            <p><strong>Phone:</strong>{{ $visitor->phone_number }}</p>
-            <p><strong>Apartment:</strong>{{ $visitor->apartment_id }}</p>
+            <p><strong>First Name: </strong> {{ $visitor->first_name }}</p>
+            <p><strong>Last Name:</strong> {{ $visitor->last_name }}</p>
+            <p><strong>Email:</strong> {{ $visitor->email }}</p>
+            <p><strong>Phone:</strong> {{ $visitor->phone_number }}</p>
+            <p><strong>Apartment:</strong> {{ $visitor->apartment_id }}</p>
 
             <p><strong>Check-in:</strong> {{ $reservation->checkin->format('l') }} {{ $reservation->checkin->format('d') }} {{ $reservation->checkin->format('F')  }}{{ $reservation->checkin->isoFormat('Y') }}</p>
             <p><strong>Check-out:</strong> {{ $reservation->checkout->format('l') }} {{ $reservation->checkout->format('d') }} {{ $reservation->checkout->format('F')  }}{{ $reservation->checkout->isoFormat('Y') }}</p>
