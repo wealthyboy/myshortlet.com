e9223f77b347911b46872e3eeedb4baafa6dc602
Jacob <jacob.atam@gmail.com>
2024-05-09 10:28:04 -0700

    fix

--- resources/views/pdf/index.blade.php
+++ resources/views/pdf/index.blade.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 
 <head>
     <title>Visitor Details</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body style="font-family: Arial, sans-serif;">
