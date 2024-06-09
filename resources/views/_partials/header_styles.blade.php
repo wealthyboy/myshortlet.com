d19e2b657c7bb17a16f670c7b2c9751901078787
Jacob <jacob.atam@gmail.com>
2024-06-09 00:38:25 -0700

    fix

--- resources/views/_partials/header_styles.blade.php
+++ resources/views/_partials/header_styles.blade.php
@@ -45,4 +45,17 @@
 
       }
    </script>
+
+   <!-- Google tag (gtag.js) -->
+   <script async src="https://www.googletagmanager.com/gtag/js?id=G-HF8HXV7C7C"></script>
+   <script>
+      window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+         dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+
+      gtag('config', 'G-HF8HXV7C7C');
+   </script>
 </head>
