1ae7fe7265e8bd6cb0177a7113eb3c53acfb43f8
Jacob <jacob.atam@gmail.com>
2024-10-26 11:17:59 +0100

    fix

--- resources/views/links/experience.blade.php
+++ resources/views/links/experience.blade.php
@@ -114,6 +114,71 @@
    </div>
 </div>
 
+<div class="container-fluid">
+   <div class="row p-1">
+      <div class="col-md-12">
+
+
+         <section id="box3" class=" opacity-0 ">
+            <div class="row bg-grey  pb-5 pt-5 position-relative">
+               <div class="col-md-7 rounded  card-background-image">
+                  <div id="r1" class="carousel slide" data-ride="carousel">
+                     <ol class="carousel-indicators">
+                        <li data-target="#r1" data-slide-to="0" class="active"></li>
+                        <!-- <li data-target="#r1" data-slide-to="1"></li>
+                        <li data-target="#r1" data-slide-to="2"></li> -->
+                     </ol>
+                     <div class="carousel-inner">
+                        <!-- <div class="carousel-item active">
+                           <img src="https://drive.google.com/thumbnail?id=16XtNpeqCSoiPVZ4KhTRb0rq72tYosN3h&sz=w1000" class="d-block w-100" alt="...">
+                        </div> -->
+                        <div class="carousel-item active">
+                           <img src="/images/banners/welcom_image.jpg" class="d-block w-100" alt="...">
+                        </div>
+
+                        <!-- <div class="carousel-item">
+                           <img src="/images/banners/r.jpeg" class="d-block w-100" alt="...">
+                        </div> -->
+                     </div>
+
+                     <!-- <button class="carousel-control-prev" data-target="#r1" data-slide="prev"><svg width="51" height="51" viewBox="0 0 21 40" xmlns="http://www.w3.org/2000/svg">
+                           <path d="M19.9 40L1.3 20 19.9 0" class="carousel-control-prev-icon" aria-hidden="true" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg><span class="sr-only">Previous</span></button>
+
+                     <button class="carousel-control-next" data-target="#r1" data-slide="next"><svg width="19" height="40" viewBox="0 0 19 40" xmlns="http://www.w3.org/2000/svg">
+                           <path d="M.1 0l18.6 20L.1 40" stroke="#FFF" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg><span class="sr-only">Next</span></button> -->
+
+                  </div>
+               </div>
+               <div class="col-md-5  text-center d-flex justify-content-center align-items-center">
+                  <div class="about-panel  bg-right-panel bg-panel-white  bg-panel p-sm-3 p-md-5">
+                     <h2 class="mb-">The Restaurant</h2>
+                     <div class="lead text-secondary"> Experience exquisite cuisine from all over the world</div>
+                     <p class="mt-4  text-left text-black light-bold">
+                        The experienced chefs creates international specialties with unique flavors. Relax with gourmet cuisine and signature cocktails or homemade tonics, all enhanced by beautiful music and gorgeous views over the city. From seasonal menus to dining experiences to satisfy any craving, see what our chefs are preparing for you. </p>
+                     <ul class="text-left">
+                        <li> <span class="">
+                           </span> <span class="list-content">Location: Ground Floor</span> </li>
+                        <li> <span class="">
+                           </span> <span class="list-content">
+                              Serves: Breakfast, Brunch, Lunch, Dinner, Dessert</span> </li>
+                        <li> <span class="">
+                           </span> <span class="list-content">Phone: +234 810 688 7920</span> 
+                           <a class=" bold-2" target="_blank" href="https://wa.me/+2348106887920" bis_skin_checked="1">
+         <i class="fab fa-whatsapp fa-2x  ml-2 text-success"></i></a>
+                           <a class="list-link" href="tel:+234 810 688 7920"></a> </li>
+                        <li> <span class="">
+                           </span> <span class="list-content">Hours: 24hrs</span> </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </div>
+   </div>
+</div>
+
 
 <!-- -------- START HEADER 4 w/ search book a ticket form ------- -->
 <header>
