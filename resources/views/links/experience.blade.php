cebeb717015219ca67513f617439c4743681bb2d
Jacob <jacob.atam@gmail.com>
2024-10-26 10:53:55 +0100

    fix

--- resources/views/links/experience.blade.php
+++ resources/views/links/experience.blade.php
@@ -9,7 +9,7 @@
 
 
          <section id="box3" class=" opacity-0 ">
-            <div class="row   pb-5 pt-5 position-relative">
+            <div class="row bg-grey  pb-5 pt-5 position-relative">
                <div class="col-md-7 rounded  card-background-image">
                   <div id="r1" class="carousel slide" data-ride="carousel">
                      <ol class="carousel-indicators">
