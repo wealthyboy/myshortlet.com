1e75138e04889709d78765beb5ae8cc66a5d3068
Jacob <jacob.atam@gmail.com>
2024-11-07 13:41:07 +0100

    fix

--- app/Http/Controllers/Admin/Reservations/ReservationsController.php
+++ app/Http/Controllers/Admin/Reservations/ReservationsController.php
@@ -35,7 +35,7 @@ class ReservationsController extends Controller
 
 		$today = Carbon::today();
 
-        dd(Reservation::doesntHave('userReservation')->get());
+        dd(Reservation::doesntHave('user_reservation')->get());
 		
         $todaysReservations = Reservation::whereDate('checkin', $today)->get();
 
