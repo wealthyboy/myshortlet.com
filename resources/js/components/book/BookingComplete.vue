9bdb0440abd002954f2ed4f2408f14f0bc6f9e77
Jacob <jacob.atam@gmail.com>
2024-11-29 08:02:54 +0100

    fix

--- resources/js/components/book/BookingComplete.vue
+++ resources/js/components/book/BookingComplete.vue
@@ -5,8 +5,7 @@
     <div class="error-page text-center">
       <h1 class="bold-3">CONFIRMED</h1>
       <p class="large text-black">
-        Thank you for choosing Avenue montaigne for your stay. We are
-        pleased to inform you that your reservation request is CONFIRMED
+        Thank you for choosing Avenue montaigne for your stay. We are pleased to inform you that your reservation is CONFIRMED. Check your email for receipt and check-in process.
 
       </p>
 
