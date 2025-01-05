e8e959d56a19f116cc2c433543200861f3c2a35a
Jacob <jacob.atam@gmail.com>
2025-01-05 06:42:30 +0100

    fix

--- resources/js/components/properties/IndexApartments.vue
+++ resources/js/components/properties/IndexApartments.vue
@@ -416,6 +416,15 @@ export default {
             this.form.checkout = retrievedObject.checkout
             console.log(this.form.checkout)
             //this.checkAvailabity()
+        } else {
+            const urlParams = new URLSearchParams(window.location.search);
+            const checkin = urlParams.get('checkin') || ''; 
+            const checkout = urlParams.get('checkout') || ''; 
+            this.form.checkin = checkin;
+            this.form.checkout = checkout;
+            
+            const fallbackParams = JSON.stringify({ checkin, checkout });
+            localStorage.setItem('searchParams', fallbackParams);
         }
 
         jQuery(function () {
