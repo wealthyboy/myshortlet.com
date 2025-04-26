35af82531a578c0593bd7840359be5c1975d67ce
Jacob <jacob.atam@gmail.com>
2025-04-26 20:35:41 +0100

    fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,13 +5,28 @@
 @section('content')
 
 <div class="d-flex flex-wrap gap-3">
-    <a href="/admin/abandoned-carts" class="btn btn-outline-primary rounded-pill px-4 py-2">Abandoned Checkout</a>
-    <a href="/admin/apartments" class="btn btn-outline-primary rounded-pill px-4 py-2">Apartments</a>
-    <a href="/admin/reservations?coming_from=checkin" class="btn btn-outline-primary rounded-pill px-4 py-2">Check-in</a>
-    <a href="/admin/peak_periods" class="btn btn-outline-primary rounded-pill px-4 py-2">Peak Periods</a>
-    <a href="/admin/reservations?coming_from=payment" class="btn btn-outline-primary rounded-pill px-4 py-2">Reservations</a>
-    <a href="/admin/visits" class="btn btn-outline-primary rounded-pill px-4 py-2">Visits</a>
-    <a href="/admin/vouchers" class="btn btn-outline-primary rounded-pill px-4 py-2">Vouchers</a>
+    <h6>
+        <a href="/admin/abandoned-carts" class="px-4 py-2 border">Abandoned Checkout</a>
+    </h6>
+    <h6>
+        <a href="/admin/apartments" class="px-4 py-2 border">Apartments</a>
+    </h6>
+    <h6>
+        <a href="/admin/reservations?coming_from=checkin" class="px-4 py-2 border">Check-in</a>
+    </h6>
+    <h6>
+        <a href="/admin/peak_periods" class="px-4 py-2 border">Peak Periods</a>
+    </h6>
+    <h6>
+        <a href="/admin/visits" class="px-4 py-2 border">Reservations</a>
+    </h6>
+    <h6>
+        <a href="/admin/reservations?coming_from=payment" class="px-4 py-2 border">Visits</a>
+    </h6>
+    <h6>
+        <a href="/admin/vouchers" class="px-4 py-2 border">Vouchers</a>
+    </h6>
+
 </div>
 
 
