350de3b1cb59308b1681bdc4a6c55d10f0852995
Jacob <jacob.atam@gmail.com>
2025-08-08 19:01:04 +0100

    fix

--- config/mail.php
+++ config/mail.php
@@ -35,14 +35,15 @@ return [
 
 
     'mailers' => [
-         'smtp' => [
+        'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.zeptomail.com'),
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
+            'auth_mode' => null,
         ],
 
         'ses' => [
