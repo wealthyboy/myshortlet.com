c1d2bbdc9160468bd1d6b1f44ddd134b11578ee2
Jacob <jacob.atam@gmail.com>
2025-08-11 15:03:35 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,19 @@
 
 cd /home/forge/avenuemontaigne.ng || exit
 
-# Use the full path to git just in case
-/usr/bin/git pull origin master
+# Stash local changes to avoid conflicts
+/usr/bin/git reset --hard
+/usr/bin/git clean -fd
+/usr/bin/git fetch origin master
+/usr/bin/git reset --hard origin/master
 
-# Use the full path to composer
 /usr/local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
-# Restart PHP-FPM
 ( flock -w 10 9 || exit 1
     echo 'Restarting FPM...'; sudo -S service php8.2-fpm reload ) 9>/tmp/fpmlock
 
-# Run migrations
 if [ -f artisan ]; then
     /usr/bin/php artisan migrate --force
 fi
 
-# Build assets
 /usr/bin/npm run prod
