259f4c9b528ce7b3ffcde0339cde7f1ab6be4c5f
Jacob <jacob.atam@gmail.com>
2025-11-12 23:45:04 +0100

    fix

--- resources/views/emails/receipt/index.blade.php
+++ resources/views/emails/receipt/index.blade.php
@@ -728,7 +728,7 @@
                                                                                        </tr>
                                                                                        <tr>
                                                                                           <td class="text" data-color="#000000" data-fontsize="13" data-fontweight="400" data-letterspacing="0.05" data-lineheight="20" data-color="" data-align="left" style="text-align:left; font-family: 'Open Sans', Arial, Helvetica, sans-serif; font-size:13px; line-height: 20px; text-decoration: none; color: #444444; font-weight:400;" data-size="img-left-text-size" data-color="img-left-text-color" data-link-color="img-left-link-color" data-link-style="color: blue;">
-                                                                                             <b>{{ $user_reservation->currency ?? '₦' }}{{ ($reservation->price * $reservation->rate) }} per night</b>
+                                                                                             <b>{{ $user_reservation->currency ?? '₦' }}{{ $reservation->price }} per night</b>
                                                                                           </td>
                                                                                        </tr>
                                                                                        <tr>
