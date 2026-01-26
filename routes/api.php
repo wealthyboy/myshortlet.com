113df87568601c187ee8715ed4f8d42bcf6ef082
Jacob <jacob.atam@gmail.com>
2026-01-26 05:56:03 +0100

    fix

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/api/mapping_details',  'Pages\PageController@map');
+Route::post('/api/mapping_details',  'Pages\PageController@map');
